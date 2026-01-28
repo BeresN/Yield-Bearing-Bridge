c1ca7e708cd77ae6ce54a9e9dc395b54fb48d195
BeresN <kroljakub022@gmail.com>
2026-01-28 18:30:43 +0100

    added slippage protection

--- src/source/BridgeBank.sol
+++ src/source/BridgeBank.sol
@@ -16,40 +16,26 @@ import {BridgeTypes} from "../libraries/BridgeTypes.sol";
 contract BridgeBank is VaultAdapter, Ownable, Pausable {
     using SafeTransferLib for ERC20;
 
-    /*//////////////////////////////////////////////////////////////
-                            STATE VARIABLES
-    //////////////////////////////////////////////////////////////*/
+    error SlipageTooHigh();
 
     uint256 public depositNonce;
     mapping(uint256 => BridgeTypes.DepositRecord) public deposits;
-
-    /*//////////////////////////////////////////////////////////////
-                              CONSTRUCTOR
-    //////////////////////////////////////////////////////////////*/
 
     constructor(
         address vault_,
         address owner_
     ) VaultAdapter(vault_) Ownable(owner_) {}
 
-    /*//////////////////////////////////////////////////////////////
-                          DEPOSIT FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
-    /// @notice Deposits tokens to bridge and routes to vault
     function deposit(
         BridgeTypes.DepositParams calldata params
     ) external whenNotPaused returns (uint256 nonce) {
         if (params.recipient == address(0)) revert BridgeTypes.ZeroAddress();
         if (params.amount == 0) revert BridgeTypes.ZeroAmount();
 
-        // Transfer tokens from depositor
         depositToken.safeTransferFrom(msg.sender, address(this), params.amount);
 
-        // Deposit to vault and get shares
         uint256 shares = _depositToVault(params.amount);
 
-        // Create deposit record
         nonce = ++depositNonce;
         deposits[nonce] = BridgeTypes.DepositRecord({
             depositor: msg.sender,
@@ -73,28 +59,22 @@ contract BridgeBank is VaultAdapter, Ownable, Pausable {
         );
     }
 
-    /*//////////////////////////////////////////////////////////////
-                          REFUND FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
-    /// @notice Refunds a pending deposit back to the depositor
-    function refund(uint256 nonce) external onlyOwner {
+    function refund(uint256 nonce, uint256 minAmount) external onlyOwner {
         BridgeTypes.DepositRecord storage record = deposits[nonce];
 
         if (record.status != BridgeTypes.DepositStatus.Pending) {
             revert BridgeTypes.NonceAlreadyUsed(nonce);
         }
 
-        // Update status
         record.status = BridgeTypes.DepositStatus.Refunded;
 
-        // Redeem shares and send assets to depositor
         uint256 assets = _redeemFromVault(record.shares, record.depositor);
-
+        if (assets < minAmount) {
+            revert SlipageTooHigh();
+        }
         emit BridgeTypes.Refunded(record.depositor, assets, nonce);
     }
 
-    /// @notice Called by relayer to mark deposit as completed (prevents refund)
     function markCompleted(uint256 nonce) external onlyOwner {
         BridgeTypes.DepositRecord storage record = deposits[nonce];
 
@@ -105,18 +85,12 @@ contract BridgeBank is VaultAdapter, Ownable, Pausable {
         record.status = BridgeTypes.DepositStatus.Completed;
     }
 
-    /*//////////////////////////////////////////////////////////////
-                          VIEW FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
-    /// @notice Gets deposit details by nonce
     function getDeposit(
         uint256 nonce
     ) external view returns (BridgeTypes.DepositRecord memory) {
         return deposits[nonce];
     }
 
-    /// @notice Gets current value of a deposit's shares in the vault
     function getDepositValue(uint256 nonce) external view returns (uint256) {
         return _getShareValue(deposits[nonce].shares);
     }
